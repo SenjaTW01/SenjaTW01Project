dc6c83b61842c631c535a63d5926bb03dc863e9b
SenjaTW01 <109809175+SenjaTW01@users.noreply.github.com>
2022-09-21 14:24:00 +0700

    Create pusing.cpp

---
+++ pusing.cpp
@@ -0,0 +1,16 @@
+int jumlah;
+char huruf [10],nis [100];
+float nilai;
+printf("masukan sebuah bilangan bulat : ");
+scanf("%d", &jumlah);
+printf("masukan sebuah karakter : "); scanf("%s", &huruf);
+printf("masukan nis anda : ");
+scanf("%s", &nis);
+printf("masukan sebuah bilangan pecahan : ");
+scanf("%f",&nilai);
+printf("\nNilai veriable yang anda masukan adalah :\n"); printf("jumlah = %d\n", jumlah);
+printf("huruf = %s\n", huruf);
+printf("nis = ts \n", nis);
+printf("pecahan = %f \n",nilai);
+getch();
+}
