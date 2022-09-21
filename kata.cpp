2234311bd73b5e232023de577f2e2d3833ea0d90
SenjaTW01 <109809175+SenjaTW01@users.noreply.github.com>
2022-09-21 13:01:41 +0700

    Create kata.cpp

---
+++ kata.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+
+#include <conio.h> 
+
+#include <string.h> 
+
+#include <iostream.h> 
+
+main() 
+
+{ 
+
+char kata[20]; 
+
+clrscr(); 
+
+cout<<"Masukan Sembarang Kata dengan Huruf Besar ="; 
+
+gets(kata); 
+
+strlwr(kata); 
+
+cout<<"Hasil Perubahan = "<<kata; 
+
+dapatkan();
+}
