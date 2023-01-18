c89339c3bf110c1f6e519427debfa9341b900740
SenjaTW01 <109809175+SenjaTW01@users.noreply.github.com>
2023-01-18 08:24:45 +0700

    Add files via upload

---
+++ head.cpp
@@ -0,0 +1,15 @@
+#include <conio.h>
+#include <iostream.h>
+
+head(char jud[50]=""){
+	cout<<"***************************************************************"<<endl;
+   cout<<".....................::INFORMASI::....................."<<endl;
+	cout<<"                       "<<jud<<endl;
+  	cout<<"***************************************************************"<<endl;
+    	cout<<"JUDUL        : Tugas membuat program login"<<endl;
+   	cout<<"NAMA          : SENDI JA"<<endl;
+   	cout<<"KELAS          : X-PPLG"<<endl;
+   	cout<<"USERNAME : admin1122"<<endl;
+       cout<<"PASSWORD  : cobasaja"<<endl;
+   	cout<<"============================================================"<<endl;
+   }
